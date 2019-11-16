4927413722589435a5bff60544952934c7e90f21
Slava <slarostovsky@gmail.com>
2019-11-16 10:21:00 +0300

    Second

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 window.addEventListener('DOMContentLoaded', function () {
     "use strict";
 
+    //tabs
+
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
@@ -33,5 +35,68 @@ window.addEventListener('DOMContentLoaded', function () {
            }
         }
     });
+
+
+    let content = document.querySelectorAll('.content')[0];
+
+    console.log(content.scrollTop);
+    
+
+
+    // timer
+
+    let deadline = '2019-11-21';
+
+    function getTimeRemaining(endtime){
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000 ) % 60),
+            minutes = Math.floor((t / 1000 / 60 ) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60))% 24),
+            day = Math.floor((t/(1000 * 60 * 60 * 24) ));
+
+            return {
+                'total' : t,
+                'day' : day,
+                'hours' : hours,
+                'minutes' : minutes,
+                'seconds' : seconds
+            };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            day = timer.querySelector('.day'),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock(){
+            let t = getTimeRemaining(endtime);
+
+                function addZero(num){
+                    if (num <= 9){
+                        return '0' + num;
+                    }else return num;
+                }
+                day.textContent = addZero(t.day);
+                hours.textContent = addZero(t.hours);
+                minutes.textContent = addZero(t.minutes);
+                seconds.textContent = addZero(t.seconds);
+
+            
+
+            if (t.total <= 0){
+                clearInterval(timeInterval);
+                day.textContent = '00';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+
+    }
+
+    setClock('timer', deadline);
 
 });
